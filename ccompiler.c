8c39d342eb80077ef2567bb50471ec6d5c146619
Nobu Komichi <nobukomichi@gmail.com>
2022-08-13 14:10:22 +0900

    Add integer add and sub

--- ccompiler.c
+++ ccompiler.c
@@ -7,10 +7,28 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf("    .globl _main\n");
     printf("    .p2align 2\n");
     printf("_main:\n");
-    printf("    mov x0, %d\n", atoi(argv[1]));
+    printf("    mov x0, #%ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add w0, w0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            printf("    sub w0, w0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
     return 0;
 }
